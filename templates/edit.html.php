e7f4dadf1c21898876f54c90f86726e4d77acac3
Øyvind Rangøy <rangoy@pns.no>
2016-09-04 10:51:59 +0200

    A touch of MVC: separated the html templates from the code and placed in separate directory.

---
+++ templates/edit.html.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="wodotexteditor/wodotexteditor.js" type="text/javascript" charset="utf-8"></script>
+    <script src="editor.js" type="text/javascript" charset="utf-8"></script>
+    <script>
+        // Set variables used by the editor in editor.js
+       var odfLoadUrl="openbyid.php?id=<?php echo $id?>";               
+       var odfEditorFullname = "Øyvind Rangøy";
+       var odfEditorColor = "green";       
+       var odfMeta = [];<?php              
+               echo "\n"; 
+               foreach ($meta as $key => $value) {               
+               echo '         odfMeta["'.$key.'"] = "'.$value.'";'."\n";
+             } ?>              
+
+       $(document).ready(function () {
+           
+       
+       $('.changeable').keyup(function () { 
+               // On change:  if value matches initially loaded value, 
+               // mark as "unchanged", else mark as "changed"               
+               if($(this).val()==odfMeta[$(this).attr('id')]){
+                 $(this).removeClass('changed').addClass('unchanged');
+               } else { 
+                $(this).removeClass('unchanged').addClass('changed');
+               }                                             
+           });
+       });      
+          
+    </script>
+    <!-- editor: end -->
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body onload="createEditor()" style="height: 100%">
+<form  style="width: 100%; display: flex; height: 100%; flex-flow: column;">
+    <div style="padding: 5px">
+        <h1 style="padding:0px; maring:0px; display: inline-block">Rediger dokument</h1>
+        <a style="vertical-align: top; float: right;" href="."><img src="images/exit.png"></a>
+
+        
+    </div>
+    <div style="flex: 0 1 auto; background: #f3f3f3; padding: 10px" >
+    <input type=hidden name=id id=id value=<?php echo $id?> >
+    <label for=title>Tittel:</label> <input class="changeable unchanged" name=title id=title type=text value="<?php echo $meta['title']?>">
+    <label for=creatorname>Forfatter:</label> <input class="changeable unchanged" name=creatorname id=creatorname type=text value="<?php echo $meta['creatorname']?>">
+    </div>
+    <div id="editorContainer" style="flex: 1 1 auto"></div>
+</form>
+</body>
+</html>
---
+++ templates/index.html.php
@@ -0,0 +1,78 @@
+<html>
+<head>
+<link rel="stylesheet" href="css/jquery.dataTables.css">
+<link rel="stylesheet" href="css/style.css">
+<script src="js/jquery.js"></script>
+<script src="js/jquery.dataTables.min.js"></script>
+<script>
+    
+  // Setup - add a text input to each footer cell
+
+    $(document).ready(function() { 
+    $('#index tfoot th').each( function () {
+        var title = $(this).text();
+        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+    } );
+
+     var table = $('#index').DataTable();  
+    
+  // Apply the search
+    table.columns().every( function () {
+        var that = this;
+ 
+        $( 'input', this.footer() ).on( 'keyup change', function () {
+            if ( that.search() !== this.value ) {
+                that
+                    .search( this.value )
+                    .draw();
+            }
+        } );
+    } );            
+    } ); 
+
+</script>
+</head>
+
+<body>
+<h1 style="display: inline-block">Dokumenter</h1>
+<a style="vertical-align: top; float: right;" href="edit.php"><img src="images/new.png"></a>
+
+<table id="index" class="stripe" cellspacing="0">
+<thead>
+    <th>Id</th>
+    <th>Tittel</th>
+    <th>Av</th>
+    <th>Tid</th>
+    <th>Størrelse</th>
+</thead>
+<tfoot>
+    <th>Id</th>
+    <th>Tittel</th>
+    <th>Av</th>
+    <th>Tid</th>
+    <th>Størrelse</th>
+</tfoot>
+
+<tbody>
+    <?php foreach($indexlist as $item) { ?>
+        <tr>
+            <?php foreach($item as $name=>$value) { ?>
+                <td class="alternating">
+                    <?php 
+                    if($name=="id")
+                        echo 
+                        "<a href=\"ViewerJS/#../openbyid.php?id=".intval($value)."\"><img class=\"icon\" src=\"images/view.png\" /></a>
+                        <a href=\"edit.php?id=".intval($value)."\"><img class=\"icon\" src=\"images/edit.png\" /></a>
+                        <a href=\"openbyid.php?id=".intval($value)."\"><img class=\"icon\" src=\"images/dl.png\" /></a>                         
+                        <a onclick=\"return confirm('Sletting kan ikke gjenopprettes. Er du sikker?')\"  href=\"deletebyid.php?id=".intval($value)."\"><img class=\"icon\" src=\"images/delete.png\" /></a>";                         
+                    else                    
+                        echo $value;                    
+                    ?>
+                </td>
+            <?php } ?>
+        </tr>
+    <?php } ?>   
+</tbody>
+</table>
+</body>
+</html>
