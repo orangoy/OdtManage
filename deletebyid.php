1c6bc04f0269fbaea48710c175eb9f6dcd35bf34
Øyvind Rangøy <rangoy@pns.no>
2016-09-06 20:28:43 +0200

    Some errormessages were not translated.

--- deletebyid.php
+++ deletebyid.php
@@ -1,7 +1,7 @@
 <?php include "bootstrap.php";
 
 // Get ID, default to 1
-if(!isset($_GET['id']) || intval($_GET['id']) <= 0) die("Feil: Ingen ID\n"); 
+if(!isset($_GET['id']) || intval($_GET['id']) <= 0) die("Error: No ID\n"); 
 else $id = intval($_GET['id']);
 
 // Check that exists
@@ -9,7 +9,7 @@ try {
     $stmt = $db->prepare("select id from odfiles where id=:id");
     $stmt->bindParam(':id', $id, PDO::PARAM_INT);
     $stmt->execute();    
-    if (! $stmt->fetch()) die("Feil: id finnes ikke\n");
+    if (! $stmt->fetch()) die("Error: ID does not exist\n");
 } catch(PDOException $e) {
     echo $e->getMessage();
 }
